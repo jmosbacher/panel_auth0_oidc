c33e23b6e849f423506e8565e508ee887aa6ca44
Yossi Mosbacher <joe.mosbacher>
2021-08-23 16:40:17 +0300

    add audience

--- panel_auth0_oidc/panel_auth0_oidc.py
+++ panel_auth0_oidc/panel_auth0_oidc.py
@@ -2,21 +2,25 @@
 from tornado.auth import OAuth2Mixin
 from panel.auth import OAuthIDTokenLoginHandler
 from panel.config import config
+import logging
 
+log = logging.getLogger(__name__)
 
 class OidcAuth0Handler(OAuthIDTokenLoginHandler, OAuth2Mixin):
 
+    _AUDIENCE = None
+    
     _SCOPE = ['openid', 'email', 'profile']
 
     _USER_KEY = 'email'
 
     _EXTRA_TOKEN_PARAMS = {
         'grant_type':    'authorization_code',
-        'response_type': 'code,token,id_token'
     }
     
     _EXTRA_AUTHORIZE_PARAMS = {
-        'grant_type': 'authorization_code'
+        'grant_type': 'authorization_code',
+        'audience': "https://api.pmts.xenonnt.org",
     }
 
     _OAUTH_ACCESS_TOKEN_URL_ = 'https://{0}.auth0.com/oauth/token'
@@ -37,3 +41,51 @@ class OidcAuth0Handler(OAuthIDTokenLoginHandler, OAuth2Mixin):
     def _OAUTH_USER_URL(self):
         url = config.oauth_extra_params.get('subdomain', 'example')
         return self._OAUTH_USER_URL_.format(url)
+
+    async def get_authenticated_user(self, redirect_uri, client_id, state,
+                                     client_secret=None, code=None):
+        """
+        Fetches the authenticated user
+
+        Arguments
+        ---------
+        redirect_uri: (str)
+          The OAuth redirect URI
+        client_id: (str)
+          The OAuth client ID
+        state: (str)
+          The unguessable random string to protect against
+          cross-site request forgery attacks
+        client_secret: (str, optional)
+          The client secret
+        code: (str, optional)
+          The response code from the server
+        """
+        if code:
+            return await self._fetch_access_token(
+                code,
+                redirect_uri,
+                client_id,
+                client_secret
+            )
+
+        params = {
+            'redirect_uri': redirect_uri,
+            'client_id':    client_id,
+            'client_secret': client_secret,
+            'extra_params': {
+                'state': state,
+            },
+        }
+        if self._SCOPE is not None:
+            params['scope'] = self._SCOPE
+        if 'scope' in config.oauth_extra_params:
+            params['scope'] = config.oauth_extra_params['scope']
+
+        if self._AUDIENCE is not None:
+            params['extra_params']['audience'] = self._AUDIENCE
+        if 'audience' in config.oauth_extra_params:
+            params['extra_params']['audience'] = config.oauth_extra_params['audience']
+
+        log.debug("%s making authorize request" % type(self).__name__)
+        self.authorize_redirect(**params)
